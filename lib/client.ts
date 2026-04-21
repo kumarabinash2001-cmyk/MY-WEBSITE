9723610b4d0b59a7d6189a586c2b105fe64be8a4
kumarabinash2001-cmyk <kumarabinash2001@gmail.com>
2026-04-21 22:17:24 +0530

    Add hasEnvVars to check Supabase environment variables
    Added hasEnvVars constant to check for environment variables.

--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// This is the cn function we added before
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// ADD THIS NEW PART BELOW TO FIX THE CURRENT ERROR
+export const hasEnvVars = 
+  process.env.NEXT_PUBLIC_SUPABASE_URL && 
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
